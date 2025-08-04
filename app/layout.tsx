ab462670c8d01b2ff697baec664f71328fc90893
krazy8 <onlykrazy8@gmail.com>
2025-08-04 17:17:19 +0100

    feat:fixed smartsupp issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    // <html lang="en">
+    <html lang="en">
       <body>
         {/* Smartsupp Live Chat script */}
         <Script id="smartsupp-livechat" strategy="afterInteractive">
